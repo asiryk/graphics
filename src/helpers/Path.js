fafdb127a55b1c0ba9b862459f5e30d9dc050731
asiryk <unigenocity@gmail.com>
2021-11-01 01:07:22 +0300

    Create Path class

---
+++ src/helpers/Path.js
@@ -0,0 +1,38 @@
+import { circlePoint } from "./utils";
+
+export default class Path extends Path2D {
+  constructor(thickness = 2, color = "black") {
+    super();
+    this.thickness = thickness;
+    this.color = color;
+  }
+
+  moveTo(x, y) {
+    // todo add transformations
+    super.moveTo(x, y);
+  }
+
+  lineTo(x, y) {
+    // todo add transformations
+    super.lineTo(x, y);
+  }
+
+  line(pointFrom, pointTo) {
+    this.moveTo(...pointFrom);
+    this.lineTo(...pointTo);
+  }
+
+  arc(x, y, radius, startAngle, endAngle, anticlockwise) {
+    const oneDeg = Math.PI / 180;
+    for (let theta = startAngle; theta < endAngle; theta += oneDeg) {
+      this.moveTo(...circlePoint(x, y, radius, theta, !anticlockwise));
+      this.lineTo(...circlePoint(x, y, radius, theta + oneDeg, !anticlockwise));
+    }
+  }
+
+  render(ctx) {
+    ctx.lineWidth = this.thickness;
+    ctx.strokeStyle = this.color;
+    ctx.stroke(this);
+  }
+}
